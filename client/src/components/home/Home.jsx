f7898a9d98121407c20575afcdd046d1d0af65f7
sadikmazumder <sadikmazumder@gmail.com>
2023-05-11 01:41:21 +0530

    Home component design is done

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,11 +1,21 @@
 //component
 import NavBar from "./NavBar";
+import Banner from "./Banner";
+import { Box, styled } from "@mui/material";
+
+const Component = styled(Box)`
+  padding: 20px 10px;
+  background: #f2f2f2;
+`;
 
 const Home = () => {
   return (
-    <div>
+    <>
       <NavBar />
-    </div>
+      <Box>
+        <Banner />
+      </Box>
+    </>
   );
 };
 
